582fc614617fb6bfe0c311e384c1e10b50109678
LeoLYW12138 <leo62227@gmail.com>
2025-01-08 21:58:51 -0500

    update task1

--- Jan06/Task1_Python_script.py
+++ Jan06/Task1_Python_script.py
@@ -2,11 +2,14 @@
 # Url: https://medium.com/@subashgandyer/papa-what-is-a-neural-network-c5e5cc427c7
 
 import os
-import requests
 import re
+
 # Code here - Import BeautifulSoup library
 import sys
-from bs4 import BeautifulSoup
+
+import requests
+from bs4 import BeautifulSoup, Tag
+
 # Code ends here
 
 # function to get the html source text of the medium article
@@ -20,8 +23,8 @@ def get_page():
     # Code ends here
 
     # handling possible error
-    if not re.match(r'https?://medium.com/', url):
-        print('Please enter a valid website, or make sure it is a medium article')
+    if not re.match(r"https?://medium.com/", url):
+        print("Please enter a valid website, or make sure it is a medium article")
         sys.exit(1)
 
     # Code here - Call get method in requests object, pass url and collect it in res
@@ -29,49 +32,67 @@ def get_page():
     # Code ends here
 
     res.raise_for_status()
-    soup = BeautifulSoup(res.text, 'html.parser')
+    soup = BeautifulSoup(res.content, "html.parser")
     return soup
+
 
 # function to remove all the html tags and replace some with specific strings
 
 
 def clean(text):
-    rep = {"<br>": "\n", "<br/>": "\n", "<li>":  "\n"}
+    rep = {"<br>": "\n", "<br/>": "\n", "<li>": "\n"}
     rep = dict((re.escape(k), v) for k, v in rep.items())
     pattern = re.compile("|".join(rep.keys()))
     text = pattern.sub(lambda m: rep[re.escape(m.group(0))], text)
-    text = re.sub('\<(.*?)\>', '', text)
+    text = re.sub("\<(.*?)\>", "", text)
     return text
 
 
+def format_content(content: Tag):
+    content.select_one("div.speechify-ignore").decompose()
+    article = content.select_one("article")
+    elements = article.find_all(["h1", "h2", "h3", "h4", "h5", "h6", "p"])
+    markdown_content = f"url: {url}\n\n"
+    for element in elements:
+        if element.name.startswith("h"):
+            level = int(element.name[1])  # Get the header level
+            markdown_content += f"{'#' * level} {element.get_text()}\n\n"
+        elif element.name == "p":
+            markdown_content += f"{element.get_text()}\n\n"
+
+    return markdown_content
+
+
 def collect_text(soup):
-    text = f'url: {url}\n\n'
-    para_text = soup.find_all('p')
+    text = f"url: {url}\n\n"
+    para_text = soup.find_all("p")
     print(f"paragraphs text = \n {para_text}")
     for para in para_text:
         text += f"{para.text}\n\n"
     return text
 
+
 # function to save file in the current directory
 
 
 def save_file(text):
-    if not os.path.exists('./scraped_articles'):
-        os.mkdir('./scraped_articles')
+    if not os.path.exists("./scraped_articles"):
+        os.mkdir("./scraped_articles")
     name = url.split("/")[-1]
     print(name)
-    fname = f'scraped_articles/{name}.txt'
+    fname = f"scraped_articles/{name}.txt"
 
     # Code here - write a file using with (2 lines)
     with open(fname, "w") as f:
         f.write(text)
     # Code ends here
 
-    print(f'File saved in directory {fname}')
+    print(f"File saved in directory {fname}")
 
 
-if __name__ == '__main__':
-    text = collect_text(get_page())
-    save_file(text)
+if __name__ == "__main__":
+    page = get_page()
+    formatted = format_content(page)
+    save_file(formatted)
     # Instructions to Run this python code
     # Give url as https://medium.com/@subashgandyer/papa-what-is-a-neural-network-c5e5cc427c7
